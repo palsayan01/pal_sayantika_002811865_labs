6a01a6c3eb3f32cb2deab8a36d264250cccd454c
palsayan01 <144847202+palsayan01@users.noreply.github.com>
2023-09-15 20:01:07 -0400

    Add files via upload

---
+++ Lab 1/Lab1/src/model/Product.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author palsa
+ */
+public class Product {
+    private String name;
+    private String desc;
+    private String avl;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getAvl() {
+        return avl;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setAvl(String avl) {
+        this.avl = avl;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+
+
+
+    
+   
+}
