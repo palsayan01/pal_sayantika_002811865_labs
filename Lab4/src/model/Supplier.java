1157683ff627eed998ce8b34d64cddcdbb02e194
Sayantika Pal <pal.sayan@northeaster.edu>
2023-11-05 21:40:01 -0500

    Committing Lab 4 Files. Implemented basic requirements along with 3 additional features

---
+++ Lab4/src/model/Supplier.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Rushabh
+ */
+public class Supplier {
+    
+    private String supplyName;
+    private ProductCatalog productCatalog;
+    private ImageIcon logoImage;
+    private String desc;
+
+    public ImageIcon getLogoImage() {
+        return logoImage;
+    }
+
+    public void setLogoImage(ImageIcon logoImage) {
+        this.logoImage = logoImage;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    
+    
+    
+    public Supplier() {
+        productCatalog = new ProductCatalog();
+    }
+
+    public String getSupplyName() {
+        return supplyName;
+    }
+
+    public void setSupplyName(String supplyName) {
+        this.supplyName = supplyName;
+    }
+
+    public ProductCatalog getProductCatalog() {
+        return productCatalog;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return supplyName;
+    }
+    
+}
